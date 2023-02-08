a7b9d6292a7e6aa32c87cf98e8b07bfb9ad4fe71
silas-hw <silas.hayes.williams@gmail.com>
2023-02-08 18:08:09 +0000

    Add markdown editor features

---
+++ flaskr/static/js/page_markdown.js
@@ -0,0 +1,51 @@
+function insert_into_textarea(text) {
+    textarea = document.getElementById('content')
+    //IE support
+    if (document.selection) {
+        textarea.focus();
+        sel = document.selection.createRange();
+        sel.text = text;
+    }
+    //MOZILLA and others
+    else if (textarea.selectionStart || textarea.selectionStart == '0') {
+        var startPos = textarea.selectionStart;
+        var endPos = textarea.selectionEnd;
+        textarea.value = textarea.value.substring(0, startPos)
+            + text
+            + textarea.value.substring(endPos, textarea.value.length);
+
+        textarea.focus()
+        textarea.selectionEnd = endPos
+        textarea.selectionStart = endPos
+    } else {
+        textarea.value += text;
+        textarea.focus()
+        cursorPos = textarea.value.length-text.length
+        textarea.selectionEnd = cursorPos
+        textarea.selectionStart = cursorPos
+    }
+}
+
+function italic() {
+    textarea = document.getElementById('content')
+    cursorPos = insert_into_textarea('**')
+
+    textarea.selectionEnd+=1
+    textarea.selectionStart+=1
+}
+
+function bold() {
+    textarea = document.getElementById('content')
+    insert_into_textarea('****')
+
+    textarea.selectionEnd+=2
+    textarea.selectionStart+=2
+}
+
+function header(size) {
+    textarea = document.getElementById('content')
+    insert_into_textarea('#'*size)
+
+    textarea.selectionEnd+=size+1
+    textarea.selectionStart+=size+1
+}
