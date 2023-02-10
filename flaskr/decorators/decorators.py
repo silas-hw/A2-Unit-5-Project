7f7c6ddf2d982acb4fb82c01a96157ba9dbb53be
silas-hw <silas.hayes.williams@gmail.com>
2023-02-10 13:02:51 +0000

    Add decorator to check if a user is an admin

--- flaskr/decorators/decorators.py
+++ flaskr/decorators/decorators.py
@@ -3,7 +3,8 @@ import functools
 
 def check_loggedin(func):
     '''
-    This can be used to 'wrap' any route in the website to ensure only logged in users can access it
+    This can be used to 'wrap' any route in the website 
+    to ensure only logged in users can access it
     '''
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
@@ -14,11 +15,26 @@ def check_loggedin(func):
 
 def check_loggedout(func):
     '''
-    This can be used to 'wrap' any route in the website to ensure only logged in users can access it
+    This can be used to 'wrap' any route in the website 
+    to ensure only logged in users can access it
     '''
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         if 'loggedin' in session:
             return redirect(url_for('main.dashboard'))
         return func(*args, **kwargs)
+    return wrapper
+
+def check_admin(func):
+    '''
+    This can be used to 'wrap' any route in the website to 
+    ensure only admin users can access its content
+    '''
+    @functools.wraps(func)
+    def wrapper(*args, **kargs):
+        if 'loggedin' in session:
+            access_level = session['access']
+            if access_level == 3:
+                return func(*args, **kargs)
+        return redirect(url_for('main.dashboard'))
     return wrapper
